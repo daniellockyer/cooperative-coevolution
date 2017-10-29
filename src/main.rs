64143d176c041d8e5d2124390f558e9ba500be94
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2017-10-29 13:23:29 +0000

    Updated as of Sun 29 Oct 13:23:29 GMT 2017

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,22 @@ extern crate rand;
 
 mod func;
 
-fn main() {
-    
-}
+use rand::{thread_rng, Rng};
 
-/*use rand::{thread_rng, Rng};
-
+const POPULATION_SIZE: usize = 100;
 const TARGET_LEN: usize = 16;
 
 #[derive(Clone, Debug)]
 struct EvoPheno {
-    text: Vec<u8>,
+    text: Vec<bool>,
     fitness: i32
 }
 
 impl EvoPheno {
-    fn new(t: Vec<u8>) -> EvoPheno {
+    fn new(t: Vec<bool>) -> EvoPheno {
         let mut fitness: i32 = 0;
-        let target_chars = TARGET.as_bytes();
 
-        for i in 0..TARGET_LEN {
-            if target_chars[i] == t[i] {
-                fitness += 1;
-            }
-        }
+        // calculate fitness
 
         EvoPheno {
             text: t,
@@ -37,10 +29,10 @@ impl EvoPheno {
     }
 
     fn new_random() -> EvoPheno {
-        EvoPheno::new((0..TARGET_LEN).map(|_| thread_rng().gen_range(32, 127)).collect())
+        EvoPheno::new(thread_rng().gen_iter::<bool>().take(TARGET_LEN).collect::<Vec<bool>>())
     }
 
-    fn crossover(&self, other: &EvoPheno) -> EvoPheno {
+    /*fn crossover(&self, other: &EvoPheno) -> EvoPheno {
         EvoPheno::new((0..TARGET_LEN).map(|i| if rand::random::<f64>() < 0.6 {
             self.text[i]
         } else {
@@ -54,11 +46,14 @@ impl EvoPheno {
         } else {
             self.text[i]
         }).collect())
-    }
+    }*/
 }
 
 fn main() {
-    let mut population: Vec<EvoPheno> = (0..population_size).map(|_| EvoPheno::new_random()).collect();
+    let mut population: Vec<EvoPheno> =
+        (0..POPULATION_SIZE).map(|_| EvoPheno::new_random()).collect();
+
+    println!("{:#?}", population);
 
     let mut iterations = 0;
     let mut gen = 0;
@@ -72,5 +67,3 @@ fn main() {
         // evaluate fitness of each individual in Pop(gen)
     }
 }
-
-*/
