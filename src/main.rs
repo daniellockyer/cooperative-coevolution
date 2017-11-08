7964ae4b6ed8627a1bc2bbada8df6029c456255b
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2017-11-08 12:28:13 +0000

    Updated as of Wed  8 Nov 12:28:13 GMT 2017

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use func::Function;
 use std::f64;
 use std::{thread, time};
 
-const POPULATION_SIZE: usize = 40;
+const POPULATION_SIZE: usize = 100;
 const TARGET_LEN: usize = 16;
 
 #[derive(Clone, Debug)]
@@ -58,27 +58,46 @@ fn make_vec(size: u32, value: u16) -> Vec<f64> {
     (0..size).map(|_| f64::from(f16::from_bits(value))).collect()
 }
 
-fn gen_pair() -> (usize, usize) {
+fn tournament(population: &Vec<EvoPheno>) -> (usize, usize) {
     let a_index = thread_rng().gen_range(0, POPULATION_SIZE) as usize;
     let b_index = thread_rng().gen_range(0, POPULATION_SIZE) as usize;
-    (a_index, b_index)
+
+    if population[a_index].fitness < population[b_index].fitness {
+        (a_index, b_index)
+    } else {
+        (b_index, a_index)
+    }
+}
+
+fn print_pop(iterations: u32, population: &Vec<EvoPheno>) {
+    print!("{}[2J", 27 as char);
+    for p in population {
+        println!("{:?} = {}", make_vec(1, p.val), p.fitness);
+    }
+
+    let mut lowest_fitness = f64::MAX;
+    let mut best_value = f64::MAX;
+    for p in population {
+        if p.fitness < lowest_fitness {
+            lowest_fitness = p.fitness;
+            best_value = f64::from(f16::from_bits(p.val));
+        }
+    }
+    println!("\n{},{},{}", iterations, best_value, lowest_fitness);
 }
 
 fn main() {
-    let mut population: Vec<EvoPheno> = (0..POPULATION_SIZE).map(|_| EvoPheno::new(thread_rng().gen::<u16>())).collect();
+    let mut population: Vec<EvoPheno> =
+        (0..POPULATION_SIZE).map(|_| EvoPheno::new(thread_rng().gen::<u16>())).collect();
 
 //    let (function, dimensions) = (func::Rastrigin, 20);
 //    let (function, dimensions) = (func::Schwefel, 10);
-//    let (function, dimensions) = (func::Griewankg, 10);
+    let (function, dimensions) = (func::Griewangk, 10);
 //    let (function, dimensions) = (func::Ackley, 30);
-    let (function, dimensions) = (func::Rosenbrock, 10);
+//    let (function, dimensions) = (func::Rosenbrock, 2);
 
     for p in &mut population {
         p.fitness = function.calc(make_vec(dimensions, p.val));
-    }
-
-    for p in &population {
-        println!("{:?} = {}", make_vec(1, p.val), p.fitness);
     }
 
     let mut iterations = 0;
@@ -87,23 +106,11 @@ fn main() {
     println!("iteration,fitness,value");
 
     while iterations < 1000 {
-        let (a_index, b_index) = gen_pair();
-       let parent1_index = if population[a_index].fitness < population[b_index].fitness {
-            a_index
-        } else {
-            b_index
-        };
+        let (parent1_index, _) = tournament(&population);
 
         let mut child = if crossover {
-            let (c_index, d_index) = gen_pair();
-            let parent2_index = if population[c_index].fitness < population[d_index].fitness {
-                c_index
-            } else {
-                d_index
-            };
-
-            let cross_child = population[parent1_index].crossover(&population[parent2_index]);
-            cross_child.mutate()
+            let (parent2_index, _) = tournament(&population);
+            population[parent1_index].crossover(&population[parent2_index]).mutate()
         } else {
             population[parent1_index].mutate()
         };
@@ -111,34 +118,11 @@ fn main() {
         iterations += 1;
         child.fitness = function.calc(make_vec(dimensions, child.val));
 
-        let (c_index, d_index) = gen_pair();
-
-        let new_index = if population[c_index].fitness < population[d_index].fitness {
-            d_index
-        } else {
-            c_index
-        };
-
+        let (_, new_index) = tournament(&population);
         std::mem::replace(&mut population[new_index], child);
 
-        print!("{}[2J", 27 as char);
+        print_pop(iterations, &population);
 
-        for p in &population {
-            println!("{:?} = {}", make_vec(1, p.val), p.fitness);
-        }
-
-        let mut lowest_fitness = f64::MAX;
-        let mut best_value = f64::MAX;
-
-        for p in &population {
-            if p.fitness < lowest_fitness {
-                lowest_fitness = p.fitness;
-                best_value = f64::from(f16::from_bits(p.val));
-            }
-        }
-
-        println!("\n{},{},{}", iterations, best_value, lowest_fitness);
-
-        thread::sleep(time::Duration::from_millis(25));
+//        thread::sleep(time::Duration::from_millis(10));
     }
 }
